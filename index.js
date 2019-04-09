3482791d21871da4a892a8116271a98691637107
Keyur Patel <ekeyur@gmail.com>
2019-04-08 21:09:54 -0400

    completed - i think...

---
+++ index.js
@@ -0,0 +1,48 @@
+#!/usr/bin/env node
+
+const clear       = require('clear');
+const figlet      = require('figlet');
+const chalk       = require('chalk');
+const CLI         = require('clui');
+const Spinner     = CLI.Spinner;
+const args = process.argv
+
+const { errorLog, usage, fetchPrRequests } = require('./lib/utils');
+
+// clear the console and print the heading
+clear();
+console.log(
+  chalk.blue(
+    figlet.textSync('PRs', { horizontalLayout: 'full' })
+  )
+);
+
+// Throw an error if atleast 3 args are not given
+if (args.length < 4) {
+  errorLog(`Please give command in correct format`);
+  usage()
+  process.exit(1);
+}
+
+const spinner = new Spinner('Making a request, please wait...');
+
+// Create an array of requests 
+const prRequests = fetchPrRequests(args.slice(3));
+
+
+const getPRs = (urls) => {
+  spinner.start();
+  return Promise.all(urls)
+    .then(users_prs => {
+      users_prs.map(prs => {
+        spinner.stop();
+        if(prs.items[0]) console.log(`\r\n${prs.items[0] && prs.items[0].user.login}'s pull requests`)
+        prs.items.map((pr,index) => console.log(index+1 + ': '+pr.title))
+      })
+    })
+    .catch(err => {
+      errorLog(err);
+    })
+}
+
+getPRs(prRequests);
